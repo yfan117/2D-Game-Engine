64ee60e4323f59568e3c4ade9ac10bf190cc65ab
yulong fan <59940376+yfan117@users.noreply.github.com>
2020-09-17 01:18:18 -0500

    Update Display.java

--- Display.java
+++ Display.java
@@ -9,7 +9,13 @@ import javax.swing.JPanel;
 
 public class Display extends Game{
 
-
+	static boolean north;
+	static boolean south;
+	static boolean west;
+	static boolean east;
+	
+	static int picRank;
+	
 	static Draw draw = new Draw(windowX, windowY);
 
 	public Display(){
@@ -39,11 +45,16 @@ public class Display extends Game{
 				
 				newClick = true;
 				
+				north = false;
+				south = false;
+				west = false;
+				east = false;
 			
 				slopeX = Math.abs(Math.round((double)(clickedX - centerX)/(clickedY - centerY)));
 				slopeY = Math.abs(Math.round((double)(clickedY - centerY)/(clickedX - centerX)));
 				
 				//System.out.println(slopeX +" " +slopeY);
+				getDirection();
 
 			}
 		});
@@ -53,9 +64,95 @@ public class Display extends Game{
 		
 	}
 	
+	public static void getDirection() {
+		
+		int differenceX = Math.abs(centerX-clickedX);
+		int differenceY = Math.abs(centerY-clickedY);
+		
+		if((differenceX < 75) && (centerY < clickedY)) {
+			
+			south = true;
+		}
+		else if((differenceX < 75) && (centerY > clickedY)) {
+			
+			north = true;
+		}
+		else if((differenceY < 75) && (centerX < clickedX)) {
+			
+			east = true;
+		}
+		else if((differenceY < 75) && (centerX > clickedX)) {
+			
+			west = true;
+		}
+		else if((differenceX >= 75) && (centerY < clickedY) && (centerX < clickedX)) {
+			
+			south = true;
+			east = true;
+		}
+		else if((differenceX >= 75) && (centerY < clickedY) && (centerX > clickedX)) {
+			
+			
+			south = true;
+			west = true;
+		}
+		else if((differenceX >=  75) && (centerY > clickedY)&& (centerX < clickedX)) {
+			
+			north = true;
+			east = true;
+		}
+		else if((differenceX >=  75) && (centerY > clickedY)&& (centerX > clickedX)) {
+			
+			north = true;
+			west = true;
+			
+		}
+	
+		/*
+		System.out.println();
+		System.out.println("north: "+north);
+		System.out.println("south: "+south);
+		System.out.println("west: "+west);
+		System.out.println("east: "+east);
+		*/
+		if((north == true) && (west == true)) {
+			picRank = 3;
+		}
+		else if((north == true) && (east == true)) {
+			picRank = 1;
+		}
+		else if((south == true) && (west == true)) {
+			picRank = 5;
+		}
+		else if((south == true) && (east == true)) {
+			picRank = 7;
+		}
+		else if(north == true) {
+			
+			picRank = 2;
+		}
+		else if(south == true) {
+			
+			picRank = 6;
+		}
+		else if(west == true) {
+			picRank = 4;
+		}
+		else if(east == true) {
+			picRank = 0;
+		}
+		
+	}
+	
 	public void update(int x, int y) {
 		
-		draw.updateValue(x, y);
+		draw.updateValue(x, y, picRank);
+		
+		
+		
+	
+		
+	
 	}
 
 		
@@ -68,6 +165,7 @@ class Draw extends JPanel{
 	
 	static int x = 0;
 	static int y = 0;
+	static int picRank;
 
 	static int windowX;
 	static int windowY;
@@ -75,22 +173,43 @@ class Draw extends JPanel{
 	static int centerX = windowX /2 ;
 	static int centerY = windowY /2 ;
 	
+	static int picCounter = 0;
+	static int timeCounter = 0;
+	
 	public Draw(int windowX, int windowY) {
 		
 		this.windowX = windowX;
 		this.windowY = windowY;
 		
+		
 		ImageIcon icon = new ImageIcon("backGround/backGround.jpg");
 		backGround = icon.getImage();
-		spaceShip = new ImageIcon("backGround/spaceShip.png").getImage();
+		spaceShip = new ImageIcon("backGround/character.png").getImage();
 		
 	}
 	
-	public void updateValue(int x, int y) {
+	public void updateValue(int x, int y, int picRank) {
+		
+		if((this.x != x) || (this.y != y)) {
+			
+			timeCounter ++;
+			
+			if(timeCounter == 5) {
+				timeCounter = 0;
+				picCounter ++;
+				
+				if(picCounter == 3) {
+					picCounter = 0;
+				}
+			}
+		}
+		
 		this.x = x;
 		this.y = y;
-		
+		this.picRank = picRank;
 		//System.out.println(x +" "+y);
+	
+		
 		
 		repaint();
 	}
@@ -103,8 +222,9 @@ class Draw extends JPanel{
 	
 			g.drawImage(backGround, 0, 0, windowX, windowY, x, y, x + windowX, y + windowY, null);
 			
+			g.drawImage(spaceShip, windowX/2 - 50, windowY/2 - 50, windowX/2 + 50, windowY/2 + 50, picCounter*17, picRank * 17, 17*(picCounter+1), picRank * 17 + 17, null);
 
-			g.drawImage(spaceShip, windowX/2-78/2, windowY/2-74/2, null);
+			//g.drawImage(spaceShip, windowX/2-78/2, windowY/2-74/2, null);
 			
 
 		
